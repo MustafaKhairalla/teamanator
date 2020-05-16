eaa1cf24542a1a1ea815fcdb6be466ecf32ee0a5
Amelia Davis <ameliaaltman22@gmail.com>
2020-05-16 13:54:02 -0600

    Added homepage to components and wired router

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import ChooseTemplate from "./pages/ChooseTemplate";
 import MemberBuild from "./pages/MemberBuild";
+import Home from "./pages/Homepage.js";
 
 function App() {
   return (
     <Router>
       <div >
+        <Route exact path="/" component={Home} />
         <Route exact path="/template" component={ChooseTemplate} />
         <Route exact path="/member" component={MemberBuild} />
 
