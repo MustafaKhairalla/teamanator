e054d5d47366ba9638bb3ce896baef54d4e276f5
mustafa khairalla <mukhairalla@gmail.com>
2020-05-21 16:06:03 -0600

    add routes for axios in client side

---
+++ client/src/utils/API.js
@@ -0,0 +1,82 @@
+import axios from "axios";
+
+export default {
+    // Get all users
+    getUsers: function () {
+        return axios.get("api/users");
+    },
+
+    // Handle business cards
+    getAllBusinessCards: function () {
+        return axios.get("api/business")
+    },
+
+    getBusinessCardsByOwner: function (id) {
+
+    },
+
+    createBusinessCard: function (businessData) {
+        return axios.post("api/business", businessData);
+    },
+
+    DeleteBusinessCardsById: function (id) {
+        return axios.delete("api/business", + id)
+    },
+
+
+    // handle education cards ------------------------------------------
+
+    getAllEducationCards: function () {
+        return axios.get("api/education")
+    },
+
+    getEducationCardsByOwner: function () {
+
+    },
+
+    createEducationCard: function (educationData) {
+        return axios.post("api/education", educationData);
+    },
+
+    DeleteEducationCardsById: function (id) {
+        return axios.delete("api/education", + id)
+    },
+
+    // Handle fintess cards --------------------------------
+
+    getAllFitnessCards: function () {
+        return axios.get("api/fitness")
+    },
+
+    getFitnessCardsByOwner: function () {
+
+    },
+
+    createFitnessCard: function (fitnessData) {
+        return axios.post("api/fitness", fitnessData);
+    },
+
+    DeleteFitnessCardsById: function (id) {
+        return axios.delete("api/fitness", + id)
+    },
+
+    //handle sports cards---------------------------------
+
+    getAllSportCards: function () {
+        return axios.get("api/sport")
+    },
+
+    getSportCardsByOwner: function () {
+
+    },
+
+    createSportCard: function (sportData) {
+        return axios.post("api/sport", sportData);
+    },
+
+    DeleteSportCardsById: function (id) {
+        return axios.delete("api/sport", + id)
+    },
+
+
+} // End of Export function 
